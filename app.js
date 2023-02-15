9fc4212cd1b44cc65249bd2d7240daf2d5cc107c
Osanda Bandara <ranukmalshan@gmail.com>
2023-02-15 12:56:44 +0530

    allow header

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, HEAD, OPTIONS, POST, PUT, DELETE',
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  )
+  next()
+})
 
 const mongoose = require('mongoose')
 mongoose.connect(
